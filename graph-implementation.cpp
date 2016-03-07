c0a9b1db5b719fe47b8970f62709b5d3c548fcdf
sbmc7 <super.shubham.7@gmail.com>
2016-03-07 12:17:15 +0530

    Update graph-implementation.cpp
    Changing the variable name

--- graph-implementation.cpp
+++ graph-implementation.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 //-----------Graph DS start-------------------------
 typedef struct Edge{
-		int e;//Ending vertex
-		int c;//Cost of Edge
+		int to;//Ending vertex
+		int cost;//Cost of Edge
 	}Edge;
 	
 typedef struct Graph{
@@ -15,8 +15,8 @@ typedef struct Graph{
 	char *vertices; //Array for mapping vertices to lablel
 	list<Edge> *adj; //Adjacency List 
 	void init(int V);//Initializer
-	void addEdge(int s, int e);//Add edge without cost
-	void addEdge(int s, int e, int c);//Add edge with cost;
+	void addEdge(int from, int to);//Add edge without cost
+	void addEdge(int from, int to, int cost);//Add edge with cost;
 }Graph;
 
 void Graph::init(int V){
@@ -24,18 +24,18 @@ void Graph::init(int V){
 	adj = new list<Edge>[V];
 }
 
-void Graph::addEdge(int s, int e){
+void Graph::addEdge(int from, int to){
 	Edge edge;
-	edge.e = e;
-	edge.c = 0; //Make default cost zero 
-	this->adj[s].push_back(edge);
+	edge.to = to;
+	edge.cost = 0; //Make default cost zero 
+	this->adj[from].push_back(edge);
 }
 
-void Graph::addEdge(int s, int e, int c){
+void Graph::addEdge(int from, int to, int cost){
 	Edge edge;
-	edge.e = e;
-	edge.c = c;
-	this->adj[s].push_back(edge);
+	edge.to = to;
+	edge.cost = cost;
+	this->adj[from].push_back(edge);
 }
 //-----------Graph DS end-------------------------
 
@@ -44,7 +44,7 @@ void displayEdges(Graph &g){
 		list<Edge>::iterator itr;
 		for(itr=g.adj[i].begin(); itr!=g.adj[i].end(); ++itr){
 			Edge edge = *itr;
-			printf("s = %d, e = %d, c = %d\n",i,edge.e,edge.c);
+			printf("from = %d, to = %d, cost = %d\n",i,edge.to,edge.cost);
 		}
 		
 	}
@@ -64,5 +64,4 @@ int main(){
 	displayEdges(g);
 	return 0;
 }
-
 
