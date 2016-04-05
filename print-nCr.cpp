da44668c2a8144b6f51bf12b5a176d544fde1a21
sbmc7 <super.shubham.7@gmail.com>
2016-04-05 21:42:48 +0530

    Update print-nCr.cpp

--- print-nCr.cpp
+++ print-nCr.cpp
@@ -3,6 +3,18 @@
 #include<algorithm>
 
 using namespace std;
+    typedef struct limits{
+        int s,e;
+    } limits;
+void nestedForloop(int n,limits l[]){
+    //n nested loops and l[] is array of start and end of each loop
+    while(l[0].e > l[1].s){
+        for(i=0;i<n;++i){
+            printf("%d ",l[i].s);
+        }
+        printf("\n")
+    }
+}
 
 void printBasicCombinations(){
    //print all combinations for n=10 and r=3
@@ -23,5 +35,13 @@ void combinationsByPermutation(char s[],int n,int r){
 int main(){
     int n=10,r=3;
     printBasicCombinations();
+    limits l[3];
+    l[0].s=0;
+    l[0].e=2;
+    l[1].s=0;
+    l[1].e=3;
+    l[2].s=0;
+    l[2].e=4;
+    nestedForloop(3,l);
     return 0;
 }
