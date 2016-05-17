10e34e1063cfb5d37aadbd9c1f15d53ce6dc8465
sbmc7 <super.shubham.7@gmail.com>
2016-05-17 18:28:24 +0530

    Update print-nCr.cpp
    Remove nested loops

--- print-nCr.cpp
+++ print-nCr.cpp
@@ -2,20 +2,6 @@
 #include<iostream>
 #include<algorithm>
 
-using namespace std;
-    typedef struct limits{
-        int s,e;
-    } limits;
-void nestedForloop(int n,limits l[]){
-    //n nested loops and l[] is array of start and end of each loop
-    while(l[0].e > l[1].s){
-        for(i=0;i<n;++i){
-            printf("%d ",l[i].s);
-        }
-        printf("\n")
-    }
-}
-
 void printBasicCombinations(){
    //print all combinations for n=10 and r=3
    for(int i=1; i<=8; ++i){
@@ -35,13 +21,5 @@ void combinationsByPermutation(char s[],int n,int r){
 int main(){
     int n=10,r=3;
     printBasicCombinations();
-    limits l[3];
-    l[0].s=0;
-    l[0].e=2;
-    l[1].s=0;
-    l[1].e=3;
-    l[2].s=0;
-    l[2].e=4;
-    nestedForloop(3,l);
     return 0;
 }
